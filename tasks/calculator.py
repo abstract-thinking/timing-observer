80818d5385e2d39136469a9492a7558d1ea93fa8
Markus <news4max@yahoo.de>
2019-05-26 17:23:27 +0200

    Refactored code.

--- tasks/calculator.py
+++ tasks/calculator.py
@@ -1,22 +1,27 @@
 import numpy as np
 import pandas as pd
 
+# RSL 27 weeks
+PAST_WEEKS = 27
+
 
 def calculate_relative_strength(rows):
-    if len(rows) < 27:
-        raise ValueError('Length should be a least 27 entries.')
+    """
+    :param rows: [x] = week, [x][0] = date Id, [x][1] = weekly quote, [x][2] = date
+    :return: list of date id, RSL and date
+    """
+    if len(rows) < PAST_WEEKS:
+        raise ValueError('Length should be a least 27.')
 
     result = []
-    for x in range(0, len(rows) - 27):
-        #print("x=" + str(x))
+    for x in range(0, len(rows) - PAST_WEEKS):
         sum = 0
-        for y in range(x, x + 27):
-            #print("y=" + str(y))
+        for y in range(x, x + PAST_WEEKS):
             sum += rows[y][1]
 
-        rsl = rows[x][1] / (sum / 27)
+        rsl = rows[x][1] / (sum / PAST_WEEKS)
         rsl = round(rsl, 4)
-        result.append([rows[x][0], rsl])
+        result.append([rows[x][0], rsl, rows[x][2]])
 
     return result
 
