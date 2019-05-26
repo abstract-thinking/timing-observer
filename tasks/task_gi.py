7713c79d670e00ed9e1bb9b01eff744589c1c692
Markus <news4max@yahoo.de>
2019-05-26 16:11:55 +0200

    Finally refactored code.

--- tasks/task_gi.py
+++ tasks/task_gi.py
@@ -10,11 +10,10 @@ from tasks.db import get_db
 logging.basicConfig(filename='/home/markus/timing-observer/app/logs/tasks.log',
                     level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-
 INVESTMENT_FRIENDLY_MONTHS = [11, 12, 1, 2, 3, 4]
 
 
-def fetch_data():
+def fetch_data(con):
     ecb = Request('ECB')
 
     data = ecb.data('FM/B.U2.EUR.4F.KR.MRR_FR.LEV').write()
@@ -26,40 +25,30 @@ def fetch_data():
     data = ecb.data('EXR/M.USD.EUR.SP00.E').write()
     exchange_rate = data.iloc[-1][0]
 
-    db = get_db()
-
     projection = (time.strftime("%Y-%m-%d"), interest_rate, inflation_rate, exchange_rate)
     sql = '''INSERT INTO germany_indicator(date, interest_rate, inflation_rate, exchange_rate) VALUES(?,?,?,?)'''
-    db.execute(sql, projection)
-    db.commit()
-    db.close()
+    con.execute(sql, projection)
+    con.commit()
 
 
-def calculate_gi():
-    db = get_db()
+def calculate_gi(con):
     command = """SELECT * FROM germany_indicator WHERE date > '1970-01-01'"""
-    df = pd.read_sql(command, db)
+    df = pd.read_sql(command, con)
 
-    # Season
-    df['tmp_date'] = pd.to_datetime(df['date'].copy())
-    df['season_point'] = df['tmp_date'].apply(lambda d: 1 if d.month in INVESTMENT_FRIENDLY_MONTHS else 0)
-    del df['tmp_date']
-
+    df['season_point'] = df['date'].apply(lambda date: 1 if date.month in INVESTMENT_FRIENDLY_MONTHS else 0)
     df['inflation_point'] = np.where(df['inflation_rate'].lt(df['inflation_rate'].shift(12)), 1, 0)
     df['exchange_point'] = np.where(df['exchange_rate'].lt(df['exchange_rate'].shift(12)), 1, 0)
-    df['interest_point'] = calculate_interest_rate(df['interest_rate'])
-
+    df['interest_point'] = calculate_interest_rate_change(df['interest_rate'])
     df['sum_of_points'] = df['season_point'] + df['inflation_point'] + df['exchange_point'] + df['interest_point']
 
-    df.to_sql('germany_indicator', db, if_exists='replace')
-    #df = df.iloc[::-1]
-    db.close()
+    df.to_sql('germany_indicator', con, if_exists='replace')
 
 
-def calculate_interest_rate(results):
-    tmp = pd.DataFrame(results.values, columns=['decision']).diff()
+def calculate_interest_rate_change(interest_rates):
+    # TODO: How does this work exactly? Do I need a test case?
+    tmp = pd.DataFrame(interest_rates.values, columns=['decision']).diff()
 
-    tmp['decision'] = tmp['decision'].apply(lambda v: was_an_interest_rate_change(v))
+    tmp['decision'] = tmp['decision'].apply(lambda v: was_interest_rate_reduction(v))
     tmp['decision'].fillna(method='ffill', inplace=True)
     tmp['decision'].fillna(0, inplace=True)
     tmp['decision'] = tmp['decision'].astype(int)
@@ -67,17 +56,21 @@ def calculate_interest_rate(results):
     return tmp['decision'].values
 
 
-def was_an_interest_rate_change(value):
-    if value < 0:
-        return 1
-    elif value > 0:
-        return 0
+def was_interest_rate_reduction(interest_rate_change):
+    if interest_rate_change < 0:
+        return 1  # True
+    elif interest_rate_change > 0:
+        return 0  # False
     else:
         return np.NaN
 
 
 if __name__ == "__main__":
     logging.info('Starting GI task.')
-    fetch_data()
-    calculate_gi()
+    connection = get_db()
+    try:
+        fetch_data(connection)
+        calculate_gi(connection)
+    finally:
+        connection.close()
     logging.info('GI task done.')
--- tasks/task_rsl.py
+++ tasks/task_rsl.py
@@ -9,55 +9,54 @@ logging.basicConfig(filename='/home/markus/timing-observer/app/logs/tasks.log',
                     level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
-def fetch_data():
-    db = get_db()
-    indices = db.execute('SELECT id, code FROM indices').fetchall()
+def fetch_data(con):
+    indices = con.execute('SELECT id, code FROM indices').fetchall()
     for index in indices:
         data = fetch_data_with(index['code'])
         quotes = extract_data(data)
 
         if quotes is not None:
             for quote in quotes:
-                db.execute('INSERT OR IGNORE INTO dates (date) VALUES(?)', (quote['date'],))
-            db.commit()
+                con.execute('INSERT OR IGNORE INTO dates (date) VALUES(?)', (quote['date'],))
+            con.commit()
 
             for quote in quotes:
-                date_id = db.execute('SELECT id FROM dates WHERE date = ?', (quote['date'],)).fetchone()
-                db.execute('INSERT INTO quotes (close, date_id, code_id) VALUES(?, ?, ?)',
-                           (quote['close'], date_id['id'], index['id']))
-            db.commit()
-    db.close()
+                date_id = con.execute('SELECT id FROM dates WHERE date = ?', (quote['date'],)).fetchone()
+                con.execute('INSERT INTO quotes (close, date_id, code_id) VALUES(?, ?, ?)',
+                            (quote['close'], date_id['id'], index['id']))
+            con.commit()
 
 
-def calculate_rsl():
-    db = get_db()
-
-    indices = db.execute('SELECT id, code FROM indices').fetchall()
+def calculate_rsl(con):
+    indices = con.execute('SELECT id, code FROM indices').fetchall()
     for index in indices:
         index_id = index['id']
-        count = db.execute('SELECT COUNT(*) AS cnt FROM quotes '
-                           'WHERE quotes.code_id = ? GROUP BY code_id HAVING cnt >= 27',
-                           (index_id,)).fetchall()
+        count = con.execute('SELECT COUNT(*) AS cnt FROM quotes '
+                            'WHERE quotes.code_id = ? GROUP BY code_id HAVING cnt >= 27',
+                            (index_id,)).fetchall()
         if not count:
             logging.debug('No history data for index {}'.format(index['code']))
             continue
 
         logging.debug("Count for {} is {}.".format(index['code'], count[0][0]))
-        closes = db.execute('SELECT dates.id, quotes.close FROM quotes '
-                            'JOIN dates ON dates.id = quotes.date_id AND quotes.code_id = ?'
-                            'ORDER BY dates.date DESC',
-                            (index_id,)).fetchmany(count[0][0])
+        closes = con.execute('SELECT dates.id, quotes.close FROM quotes '
+                             'JOIN dates ON dates.id = quotes.date_id AND quotes.code_id = ?'
+                             'ORDER BY dates.date DESC',
+                             (index_id,)).fetchmany(count[0][0])
         rsl_results = calculate_relative_strength(closes)
 
         for rsl in rsl_results:
-            db.execute('UPDATE quotes SET rsl = ? WHERE code_id = ? AND date_id = ?',
-                       (rsl[1], index_id, rsl[0]))
-        db.commit()
-    db.close()
+            con.execute('UPDATE quotes SET rsl = ? WHERE code_id = ? AND date_id = ?',
+                        (rsl[1], index_id, rsl[0]))
+        con.commit()
 
 
 if __name__ == "__main__":
     logging.info('Starting RSL task.')
-    fetch_data()
-    calculate_rsl()
+    connection = get_db()
+    try:
+        fetch_data(connection)
+        calculate_rsl(connection)
+    finally:
+        connection.close()
     logging.info('RSL task done.')
