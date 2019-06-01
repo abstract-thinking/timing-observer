1e249a202b620784926bf55f41945de15855c74b
Markus <news4max@yahoo.de>
2019-06-01 19:01:09 +0200

    Fixed SQL command.

--- app/page.py
+++ app/page.py
@@ -55,6 +55,6 @@ def gi():
     db = get_db()
 
     germany_indicator = db.execute(
-        """SELECT * FROM germany_indicator WHERE date > '1970-01-01 ORDER BY date DESC'""").fetchall()
+        """SELECT * FROM germany_indicator WHERE date > '1970-01-01' ORDER BY date DESC""").fetchall()
 
     return render_template('page/gi.html', gi=germany_indicator)
